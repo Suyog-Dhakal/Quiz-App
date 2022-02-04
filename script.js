21c7bed1439bf5ec2e2ac3aa4d5252d337fef8ba
Suyog-Dhakal <075bct092.suyog@pcampus.edu.np>
2022-02-04 14:16:47 +0545

    question added and state managed

--- script.js
+++ script.js
@@ -16,9 +16,39 @@ const questions = [
       { text: "Chelsea", correct: false },
     ],
   },
+  {
+    question: "Which english club has won more UCL?",
+    answers: [
+      { text: "Liverpool", correct: true },
+      { text: "Manchester United", correct: false },
+      { text: "Arsenal", correct: false },
+      { text: "Chelsea", correct: false },
+    ],
+  },
+  {
+    question: "Which club has won most FA cup title ?",
+    answers: [
+      { text: "Liverpool", correct: false },
+      { text: "Manchester United", correct: false },
+      { text: "Arsenal", correct: true },
+      { text: "Chelsea", correct: false },
+    ],
+  },
+  {
+    question: "Which club stadium has the highest capacity?",
+    answers: [
+      { text: "Liverpool", correct: false },
+      { text: "Manchester United", correct: true },
+      { text: "Arsenal", correct: false },
+      { text: "Chelsea", correct: false },
+    ],
+  },
 ];
 
 startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+  setNextQuestion();
+});
 
 function startGame() {
   console.log("game started");
@@ -49,10 +79,38 @@ function showQuestion(question) {
 }
 
 function resetState() {
+  clearStatusClass(document.body);
   nextButton.classList.add("hide");
   while (answerButtonsElement.firstChild) {
     answerButtonsElement.removeChild(answerButtonsElement.firstChild);
   }
 }
 
-function selectAnswer(e) {}
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
+  if (shuffleQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove("hide");
+  } else {
+    startButton.innerText = "Restart";
+    startButton.classList.remove("hide");
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
